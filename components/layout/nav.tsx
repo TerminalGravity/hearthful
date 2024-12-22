847a46e763b3c8ced1c3248124ad9e37ed48e3b3
Jack Felke <jrinnfelke@gmail.com>
2024-12-22 19:12:58 +0100

    change text in profile drop down

--- components/layout/nav.tsx
+++ components/layout/nav.tsx
@@ -83,7 +83,7 @@ export default function Nav() {
                     className="block px-4 py-2 text-sm text-gray-700 hover:bg-gray-100"
                     onClick={() => setIsProfileOpen(false)}
                   >
-                    App Settings
+                    Settings
                   </Link>
                   <SignOutButton>
                     <button className="block w-full px-4 py-2 text-left text-sm text-gray-700 hover:bg-gray-100">
